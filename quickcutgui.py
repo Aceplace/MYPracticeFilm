542494fd7f8dc1adeb2a637e5bcfa2c06054aa3f
Michael Yeiter <Aceplace1116@gmail.com>
2019-02-01 17:37:57 -0500

    Can add marks to clip designating subclips.

--- quickcutgui.py
+++ quickcutgui.py
@@ -16,6 +16,16 @@ def to_ffmpeg_duration(duration):
     return f'{hours}:{minutes}:{seconds}'
 
 
+def pairs(seq):
+    it = iter(seq)
+    i = next(it, None)
+    j = next(it, None)
+    while i != None:
+        yield i, j
+        i = next(it, None)
+        j = next(it, None)
+
+
 class QuickCutGui(tk.Frame):
     def __init__(self, root):
         tk.Frame.__init__(self, root)
@@ -33,9 +43,27 @@ class QuickCutGui(tk.Frame):
 
         # Clip panel
         self.clip_panel = tk.Label(self)
-        self.clip_panel.grid(row=2, column=0)
+        self.clip_panel.grid(row=1, column=0)
         self.clip_panel.bind('<Button-1>', lambda event: self.focus_set())
         self.bind_all('<KeyPress>', self.handle_input)
+
+        # Subclip Listbox
+        subclip_lb_frame = tk.Frame(self)
+        subclip_lb_frame.grid(row=1, column=1, rowspan=2, sticky='NS')
+        subclip_lb_frame.grid_rowconfigure(1, weight=1)
+
+        tk.Label(self, text=f'Subclip Times').grid(row=0, column=1)
+        y_scroll = tk.Scrollbar(subclip_lb_frame, orient=tk.VERTICAL)
+        y_scroll.grid(row=1, column=1, sticky='NS')
+        x_scroll = tk.Scrollbar(subclip_lb_frame, orient=tk.HORIZONTAL)
+        x_scroll.grid(row=2, column=0, sticky='WE')
+
+        self.subclip_lb = tk.Listbox(subclip_lb_frame, xscrollcommand=x_scroll.set, yscrollcommand=y_scroll.set,
+                                       exportselection=False)
+        self.subclip_lb.grid(row=1, column=0, sticky='NS')
+        tk.Button(subclip_lb_frame, text='Delete subclip', command=self.delete_subclip).grid(row=3, column=0)
+        x_scroll.config(command=self.subclip_lb.xview)
+        y_scroll.config(command=self.subclip_lb.yview)
 
 
     def get_file(self, event):
@@ -85,7 +113,16 @@ class QuickCutGui(tk.Frame):
         self.clip_panel.image = image
 
         # Update label
-        self.clip_drop_lbl.configure(text=f'{timestamp} {self.get_seek_time_status()}')
+        self.clip_drop_lbl.configure(text=f'{timestamp} {self.seek_time_status()}')
+
+    def update_subclip_lb(self):
+        # Update image
+        self.subclip_lb.delete(0, tk.END)
+        for i, j in pairs(self.marks):
+            if j != None:
+                self.subclip_lb.insert(tk.END , f'{to_ffmpeg_duration(i)} to {to_ffmpeg_duration(j)}')
+            else:
+                self.subclip_lb.insert(tk.END, f'{to_ffmpeg_duration(i)} to ---')
 
     def handle_input(self, event):
         if not self.clip_info:
@@ -98,8 +135,9 @@ class QuickCutGui(tk.Frame):
             next_seek_time = self.current_seek_time - 5
         elif event.keysym == 'Down':
             next_seek_time = self.current_seek_time + 5
-        elif event.keysym == 'Space':
+        elif event.keysym == 'space':
             self.make_mark()
+            return
         else:
             return
 
@@ -108,15 +146,27 @@ class QuickCutGui(tk.Frame):
             self.update_clip_panel()
 
     def make_mark(self):
-        if self.current_seek_time in self.marks:
-            self.marks.remove(self.current_seek_time)
-        else:
+        if self.current_seek_time not in self.marks:
             bisect.insort(self.marks, self.current_seek_time)
         self.update_clip_panel()
+        self.update_subclip_lb()
+
+    def delete_subclip(self):
+        index = self.subclip_lb.curselection()[0]
+        index *= 2
+        if index < len(self.marks) - 1:
+            del self.marks[index:index+2]
+        else:
+            del self.marks[index]
+        self.update_subclip_lb()
 
     def seek_time_status(self):
-        pass
-
+        for i,j in pairs(self.marks):
+            if self.current_seek_time == i or self.current_seek_time == j:
+                return 'On Subclip Border'
+            if self.current_seek_time > i and j != None and self.current_seek_time < j:
+                return 'In Subclip'
+        return 'Out of Subclip'
 
 
 if __name__ == '__main__':
